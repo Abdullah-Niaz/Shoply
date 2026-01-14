0a0d3c124a05ad18cc47de1800228be14de0fbb0
Abdullah Niaz <abdullahniaz423@gmail.com>
2026-01-15 02:38:31 +0500

    media fiels config

--- category/models.py
+++ category/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=255)
     category_image = models.ImageField(
         upload_to="photos/categories/", blank=True)
@@ -15,4 +15,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.category_image
+        print("DEBUG __str__ CALLED")
+        print("TYPE:", type(self.category_name))
+        return self.category_name
+

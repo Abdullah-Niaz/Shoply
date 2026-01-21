1005cdae1768a4bc6df28d9c02917b611449ebe3
Abdullah Niaz <abdullahniaz423@gmail.com>
2026-01-21 23:11:50 +0500

    store app updated

--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from category.models import Category
+from .models import Product
 # Create your views here.
+
+
+def store(request, category_slug=None):
+    products = None
+
+    if category_slug is not None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=category,
+            is_available=True
+        )
+    else:
+        products = Product.objects.filter(is_available=True)
+
+    context = {
+        'products': products,
+        'product_count': products.count(),
+    }
+    return render(request, "store/store.html", context)

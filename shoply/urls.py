4ed342406c3899891c893f403724c8c96d5c0501
Abdullah Niaz <abdullahniaz423@gmail.com>
2026-01-31 16:38:01 +0500

    home routes modified

--- shoply/urls.py
+++ shoply/urls.py
@@ -8,6 +8,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home),
+    path("", views.home, name="home"),
     path("store/", include("store.urls"))
 ] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)

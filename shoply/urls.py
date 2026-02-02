d24a0225aabdd986021b78de9b7cdc6a44de5d8f
Abdullah Niaz <abdullahniaz423@gmail.com>
2026-02-02 23:39:12 +0500

    routes

--- shoply/urls.py
+++ shoply/urls.py
@@ -9,5 +9,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    path("store/", include("store.urls"))
+    path("store/", include("store.urls")),
+    path("cart/", include("cart.urls")),
 ] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
